7c4f8c55b2511cd49ceee42b143d6df2a5790d5f
patriciacatandi <patriciabcatandi@gmail.com>
2022-06-22 13:06:53 -0300

    fix: attumando nome variável

--- models/meio_ambiente_clima/taxa_precipitacao_alertario.sql
+++ models/meio_ambiente_clima/taxa_precipitacao_alertario.sql
@@ -40,6 +40,6 @@ SELECT * FROM temp
 {% set max_partition = run_query("SELECT DATE(gr) FROM (SELECT IF(max(data_particao) > CURRENT_DATE('America/Sao_Paulo'), CURRENT_DATE('America/Sao_Paulo'), max(data_particao)) as gr FROM `rj-cor.meio_ambiente_clima_staging.taxa_precipitacao_alertario_last_partition`)").columns[0].values()[0] %}
 
 WHERE
-    data_medicao > ("{{ max_partition }}")
+    data_particao > ("{{ max_partition }}")
 
 {% endif %}
