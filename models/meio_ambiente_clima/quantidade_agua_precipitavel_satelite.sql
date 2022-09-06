a92490370b8e9bff998d5e45017c6cf4b3fd2980
patriciacatandi <patriciabcatandi@gmail.com>
2022-09-06 16:08:06 -0300

    voltando ao estado anterior

--- models/meio_ambiente_clima/quantidade_agua_precipitavel_satelite.sql
+++ models/meio_ambiente_clima/quantidade_agua_precipitavel_satelite.sql
@@ -11,23 +11,6 @@
     )
 }}
 
-{% if is_incremental() %}
-
--- {% set max_partition = run_query(
---     "
---     SELECT IF(
---         max(data_particao) > CURRENT_DATETIME('America/Sao_Paulo'), CURRENT_DATETIME('America/Sao_Paulo'), max(data_particao)
---         ) as gr 
---     FROM `rj-cor.meio_ambiente_clima_staging.quantidade_agua_precipitavel_satelite_last_partition`
---     ").columns[0].values()[0] %}
-
-DECLARE last_update DATETIME DEFAULT (
-    SELECT IF(
-        max(data_particao) > CURRENT_DATETIME('America/Sao_Paulo'), CURRENT_DATETIME('America/Sao_Paulo'), max(data_particao)
-        )
-    FROM `rj-cor.meio_ambiente_clima_staging.quantidade_agua_precipitavel_satelite_last_partition`
-);
-
 SELECT
     CONCAT(data_particao, '_', CONCAT(SAFE_CAST(hora AS STRING), ":00:00"), '_', latitude, '_', longitude) AS primary_key,
     SAFE_CAST(longitude AS FLOAT64) longitude,
@@ -36,11 +19,22 @@ SELECT
     SAFE_CAST(CONCAT(SAFE_CAST(hora AS STRING), ":00:00") AS TIME) horario,
     SAFE_CAST(DATE_TRUNC(DATE(data_particao), day) AS DATE) data_particao,
 FROM `rj-cor.meio_ambiente_clima_staging.quantidade_agua_precipitavel_satelite`
+
+{% if is_incremental() %}
+
+{% set max_partition = run_query(
+    "
+    SELECT IF(
+        max(data_particao) > CURRENT_DATETIME('America/Sao_Paulo'), CURRENT_DATETIME('America/Sao_Paulo'), max(data_particao)
+        ) as gr 
+    FROM `rj-cor.meio_ambiente_clima_staging.quantidade_agua_precipitavel_satelite_last_partition`
+    ").columns[0].values()[0] %}
+
 WHERE 
-    ano_particao >= EXTRACT(YEAR FROM SAFE_CAST(last_update AS DATETIME)) AND
-    mes_particao >= EXTRACT(MONTH FROM last_update) AND
-    data_particao >= EXTRACT(DATE FROM last_update) AND
-    hora >= EXTRACT(HOUR FROM last_update)
+    ano_particao >= EXTRACT(YEAR FROM SAFE_CAST("{{ max_partition }}" AS DATETIME)) AND
+    mes_particao >= EXTRACT(MONTH FROM SAFE_CAST("{{ max_partition }}" AS DATETIME)) AND
+    data_particao >= EXTRACT(DATE FROM SAFE_CAST("{{ max_partition }}" AS DATETIME)) AND
+    hora >= EXTRACT(HOUR FROM SAFE_CAST("{{ max_partition }}" AS DATETIME))
 {% endif %}
 
 --rj-cor
