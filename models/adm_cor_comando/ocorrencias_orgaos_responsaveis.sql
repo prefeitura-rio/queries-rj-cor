6a283a7d2e1b614e0a378b23b90e0a3f9f76f49e
d116626 <d116626@gmail.com>
2022-10-04 14:28:14 -0300

    feat: rename cor dataset and tables

---
+++ models/adm_cor_comando/ocorrencias_orgaos_responsaveis.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        materialized='incremental',
+        partition_by={
+            "field": "data_particao",
+            "data_type": "date",
+            "granularity": "month",
+        }    
+    )
+}}
+
+SELECT
+    id_evento,
+    sigla,
+    data_chegada,
+    data_inicio,
+    data_fim,
+    descricao,
+    status,
+    data_particao
+FROM `rj-cor.adm_cor_comando_staging.ocorrencias_orgaos_responsaveis`
+WHERE data_particao < CURRENT_DATE('America/Sao_Paulo')
+
+{% if is_incremental() %}
+
+{% set max_partition = run_query("SELECT gr FROM (SELECT IF(max(data_particao) > CURRENT_DATE('America/Sao_Paulo'), CURRENT_DATE('America/Sao_Paulo'), max(data_particao)) as gr FROM " ~ this ~ ")").columns[0].values()[0] %}
+
+AND
+    data_particao > ("{{ max_partition }}")
+
+{% endif %}
---
+++ models/adm_cor_comando/procedimento_operacional_padrao_orgaos_responsaveis.sql
@@ -0,0 +1,1 @@
+SELECT * FROM `rj-cor.adm_cor_comando_staging.procedimento_operacional_padrao_orgaos_responsaveis`
