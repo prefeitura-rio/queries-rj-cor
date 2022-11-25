307f826de286204759a8be957706c6b10b315aff
jpfraga <>
2022-11-25 14:13:24 +0000

    adição no dbt de pipe do nivel da lagoa

---
+++ models/clima_fluviometro/nivel_lagoa.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized='overwrite',
+        unique_key="data_medicao",
+        partition_by={
+            "field": "data_particao",
+            "data_type": "date",
+            "granularity": "month", 
+        },
+}}
+
+SELECT
+  SAFE_CAST(SAFE.PARSE_TIMESTAMP('%d/%m/%Y %H:%M', data_hora) AS DATETIME) as data_medicao,
+  SAFE_CAST(lamina_nivel as FLOAT64) as nivel_agua,
+  SAFE_CAST(DATE_TRUNC(DATE(SAFE.PARSE_TIMESTAMP('%d/%m/%Y %H:%M', data_hora)), day) AS DATE) data_particao
+FROM `rj-cor.clima_fluviometro_staging.nivel_lagoa`
