3cad99b78e8ea85d6f28982118f8103b8b34be51
patriciacatandi <patriciabcatandi@gmail.com>
2022-12-29 16:09:12 -0300

    websirene: removendo fiscalizacao_rua e clima_fluviometro para testar

--- models/fiscalizacao_rua/enderecos_cameras.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  id_camera,
-  endereco_camera,
-  SAFE_CAST(latitude as FLOAT64) latitude,
-  SAFE_CAST(longitude as FLOAT64) longitude,
-  posicao_camera
-FROM `rj-cor.fiscalizacao_rua_staging.enderecos_cameras`
