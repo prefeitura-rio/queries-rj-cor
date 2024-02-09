ae54456edc5640879e3168689d9465263840c671
patriciacatandi <patriciabcatandi@gmail.com>
2024-02-09 18:57:29 -0300

    changing materialization to get data from new api and put on old table

--- models/adm_cor_comando/ocorrencias_orgaos_responsaveis.sql
+++ models/adm_cor_comando/ocorrencias_orgaos_responsaveis.sql
@@ -10,21 +10,27 @@
     )
 }}
 
-SELECT
-    id_evento,
-    sigla,
-    SAFE_CAST(data_chegada AS TIMESTAMP) data_chegada,
-    SAFE_CAST(data_inicio AS TIMESTAMP) data_inicio,
-    SAFE_CAST(data_fim AS TIMESTAMP) data_fim,
-    descricao,
-    status,
-    SAFE_CAST(data_particao AS DATE) data_particao,
-    CONCAT(id_evento, '_', sigla, '_', descricao) AS primary_key,
-FROM `rj-cor.adm_cor_comando_staging.ocorrencias_orgaos_responsaveis`
+WITH remove_duplicated AS (
+    SELECT
+        id_evento,
+        sigla,
+        SAFE_CAST(data_chegada AS TIMESTAMP) data_chegada,
+        SAFE_CAST(data_inicio AS TIMESTAMP) data_inicio,
+        SAFE_CAST(data_fim AS TIMESTAMP) data_fim,
+        descricao,
+        status,
+        SAFE_CAST(data_particao AS DATE) data_particao,
+        CONCAT(id_evento, '_', sigla, '_', descricao) AS primary_key,
+        row_number() OVER (PARTITION BY id_evento ORDER BY created_at DESC) AS last_update
+    FROM `rj-cor.adm_cor_comando_staging.ocorrencias_orgaos_responsaveis_nova_api`
 
-{% if is_incremental() %}
+    {% if is_incremental() %}
 
-    WHERE data_particao <= CURRENT_DATE('America/Sao_Paulo')
-    AND data_particao >= DATE_SUB(CURRENT_DATE('America/Sao_Paulo'), INTERVAL 30 day)
+        WHERE data_particao <= CAST(CURRENT_DATE('America/Sao_Paulo') AS STRING)
+        AND data_particao >= CAST(DATE_SUB(CURRENT_DATE('America/Sao_Paulo'), INTERVAL 30 day) AS STRING)
 
-{% endif %} 
+    {% endif %}
+)
+
+SELECT * EXCEPT(last_update) FROM remove_duplicated
+WHERE last_update = 1
