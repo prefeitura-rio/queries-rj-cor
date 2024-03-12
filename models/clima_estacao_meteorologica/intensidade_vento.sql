af232097df597af519877a0e788901aefaaf9d25
patriciacatandi <patriciabcatandi@gmail.com>
2024-03-12 09:49:48 -0300

    bugfix

--- models/clima_estacao_meteorologica/intensidade_vento.sql
+++ models/clima_estacao_meteorologica/intensidade_vento.sql
@@ -5,4 +5,3 @@ SELECT
   SAFE_CAST(velocidade_maxima AS FLOAT64) velocidade_maxima,
   SAFE_CAST(data_atualizacao AS DATE) data_atualizacao
 FROM `rj-cor.clima_estacao_meteorologica_staging.intensidade_vento` 
-ORDER BY id_estacao
